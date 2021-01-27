cff633a3004ef30a2f9d95368ac00ec83b17a637
mohit <mohit81682@gmail.com>
2021-01-27 20:28:20 +0530

    tryng to push express

--- src/index.js
+++ src/index.js
@@ -33,32 +33,6 @@ app.use(taskRouter);
 //     return this;
 // }
 
-// const multer = require('multer');
-// const upload = multer({
-//     dest: 'uploads',
-//     limits: {
-//         fileSize: 1000000
-//     },
-//     fileFilter(req, file, cb){
-//         console.log(file.originalname);
-//         //check if file type is pdf
-//         if(!file.originalname.endsWith('.pdf')){
-//             //cb(new Error('This file type is not valid mohit'));
-//         }
-//         if(!file.originalname.match(/\.(jpg|pdf)$/)){
-//             cb(new Error('This file type is not pdf or jpg'));
-//         }
-//         cb(undefined, file.originalname);
-//     }
-// });
-
-// app.post('/upload', upload.single('upload'),function(req,res){
-//     res.send();
-// }, (error,req,res,next) => {
-//     res.status(500).send({'error': error.message});
-// });
-
-
 app.listen(port,() => {
     console.log("sds",process.env.JWT_TOKEN_KEY);
     console.log('A REST API Web service || Server connected successfully on port ', port);
